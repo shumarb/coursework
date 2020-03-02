42bf2a762b4d4afb003ae289516cedbf46eee5ba
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-03-02 22:03:21 +0800

    Create volume.c

---
+++ cs1010/problem-set-1/exercise-1/volume.c
@@ -0,0 +1,31 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #01: volume.c
+//
+// This program reads in length, width and
+// height of a box, computes its volume.
+//
+// <SHEIKH UMAR>
+
+#include <stdio.h>
+
+int main(void) {
+
+	int length, width, height, volume;
+
+	printf("Enter length: ");
+	scanf("%d", &length);
+
+	printf("Enter width: ");
+	scanf("%d", &width);
+
+	printf("Enter height: ");
+	scanf("%d", &height);
+
+	// Correct the following two statements
+	volume = length * width * height;
+
+	// Print out volume
+	printf("Volume = %d\n", volume);
+
+	return 0;
+}
