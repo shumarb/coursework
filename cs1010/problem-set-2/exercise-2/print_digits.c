acab4ac93163e077c216de8907935e19d98781e4
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-03-23 09:54:42 +0800

    Add files via upload

---
+++ cs1010/problem-set-2/exercise-2/print_digits.c
@@ -0,0 +1,33 @@
+// CS1010 Programming Methodology
+// Problem Set 2 Exercise #02: print_digits.c
+//
+// Program reads in an integer, and prints out its digits
+//
+// SHEIKH UMAR
+#include <stdio.h>
+
+void print_digits(int num); // function prototype
+
+int main(void) {
+	int num;
+
+	printf("Enter a positive integer: ");
+	scanf("%d",&num);
+
+	print_digits(num);
+	return 0;
+}
+
+// Print digits right to left
+void print_digits(int num){
+	//Keep extracting each digit as long as num>0
+	while(num>0){
+		int n1 = num%10;
+		printf("%d ",n1);
+		//Obtain digits of num without earlier last digit
+		//While loop will rely on this value of num
+		num/=10;
+	}
+	printf("\n");  // change line
+}
+
---
+++ cs1010/problem-set-2/exercise-3/age.c
@@ -0,0 +1,32 @@
+// CS1010 Programming Methodology
+// Problem Set 2 Exercise #03: age.c
+//
+// Program reads in age, keeps asking user for it if it falls out of range.
+// Program takes note of number of attempts for correct age.
+// Program prints out age that meets criteria and number of attempts needed
+//
+// SHEIKH UMAR
+
+#include <stdio.h>
+
+int main(void) {
+	int age,counter=0;
+
+	printf("Enter age: ");
+	scanf("%d",&age);
+	counter++;
+	
+	// Keep asking for age if it falls out of range.
+	// Increase counter for every input entered.
+	while((age<1) || (age>100)){
+		printf("Enter age: ");
+		scanf("%d",&age);
+		counter++;
+	}
+
+	printf("Your age is %d\n",age);
+	printf("Number of attempts = %d\n",counter);
+
+	return 0;
+}
+
