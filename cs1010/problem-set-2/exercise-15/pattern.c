66742ff6e0bbf63627625b831405c7524095914b
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-03-26 09:29:17 +0800

    Solutions to problem sets 14, 15, 16

---
+++ cs1010/problem-set-2/exercise-15/pattern.c
@@ -0,0 +1,28 @@
+// CS1010 Programming Methodology
+// Problem Set 2 Exercise #15: pattern.c
+//
+// Program reads in a number, and prints out a pattern
+// that corresponds to the number.
+//
+// SHEIKH UMAR, A0149874L
+
+#include <stdio.h>
+
+int main(void) {
+	int count;   // number of #
+	int i,j,k=0;
+
+	printf("Enter count: ");
+	scanf("%d",&count);
+
+	for(i=1;i<=count;i++){
+		printf("#");
+		k++;
+		for(j=1;j<=k;j++){
+			printf("-");
+		}
+	}
+	printf("\n");
+	return 0;
+}
+
---
+++ cs1010/problem-set-2/exercise-16/print_square.c
@@ -0,0 +1,49 @@
+// CS1010 Programming Methodology
+// Problem Set 2 Exercise #16: print_square.c
+//
+// Program reads in a number, and prints out a square
+// that is of the size of the number.
+//
+// SHEIKH UMAR, A0149874L
+
+#include <stdio.h>
+
+int main(void) {
+	int k;
+	int i,j;
+
+	printf("Enter k: ");
+	scanf("%d",&k);
+
+	// Case 1: k=2
+	// Must hardcode as square where k=2 has no space
+	if(k==2){
+		printf("**\n");
+		printf("**\n");
+	}
+	else{
+		// First line
+		for(i=1;i<=k;i++){
+			printf("*");
+		}
+		printf("\n");
+
+		//Second line to second-last line
+		for(i=2;i<=(k-1);i++){
+			printf("*");
+			for(j=2;j<=(k-1);j++){
+				printf(" ");
+			}
+			printf("*");
+			printf("\n");
+		}
+
+		// print last line
+		for(i=1;i<=k;i++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
