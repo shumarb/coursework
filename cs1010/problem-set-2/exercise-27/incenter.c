62b69b20c25be18f3a4ec2abfeea86d6f837f679
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-04-02 12:14:37 +0800

    Update incenter.c

--- cs1010/problem-set-2/exercise-27/incenter.c
+++ cs1010/problem-set-2/exercise-27/incenter.c
@@ -3,9 +3,6 @@
 //
 // Program reads in an coordinates of 3 vertices,
 // computes and prints out the coordinates of the incenter.
-//
-//
-// SHEIKH UMAR A0149874L
 
 #include <stdio.h>
 #include <math.h>
@@ -29,7 +26,7 @@ int main(void)
 	scanf("%lf %lf", &x3, &y3);
 
 	// make function call to incenter function
-	incenter(x1,y1,x2,y2,x3,y3,&xI,&yI);
+	incenter(x1, y1, x2, y2, x3, y3, &xI, &yI);
 
 	printf("Coordinates of incenter = (%.2f, %.2f)\n", xI, yI);
 
@@ -40,14 +37,14 @@ int main(void)
 // Pass back to caller through two pointers.
 void incenter(double xA, double yA, double xB, double yB, double xC, double yC, double *xI_p, double *yI_p)
 {
-	double a,b,c,p;
-	a = length(xB,yB,xC,yC);
-	b = length(xA,yA,xC,yC);
-	c = length(xA,yA,xB,yB);
-	p = a+b+c;
+	double a, b, c, p;
+	a = length(xB, yB, xC, yC);
+	b = length(xA, yA, xC, yC);
+	c = length(xA, yA, xB, yB);
+	p = a + b + c;
 
-	*xI_p = (a*xA + b*xB + c*xC) / p;
-	*yI_p = (a*yA + b*yB + c*yC) / p;
+	*xI_p = ((a * xA) + (b * xB) + (c * xC)) / p;
+	*yI_p = ((a * yA) + (b * yB) + (c * yC)) / p;
 }
 
 // Return length of a side of triangle given its
@@ -56,8 +53,8 @@ double length(double x1, double y1, double x2, double y2)
 {
 
 	double length,x_length,y_length;
-	x_length = x1-x2;
-	y_length = y1-y2;
-	length = sqrt(pow(x_length,2.0)+pow(y_length,2.0));
+	x_length = x1 - x2;
+	y_length = y1 - y2;
+	length = sqrt(pow(x_length, 2.0) + pow(y_length, 2.0));
 	return length;  // stub
 }
