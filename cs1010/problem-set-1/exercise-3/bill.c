73975afe5908e3d55be0928284b3a9dce22c96b8
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-04-04 12:46:54 +0800

    Update bill.c

--- cs1010/problem-set-1/exercise-3/bill.c
+++ cs1010/problem-set-1/exercise-3/bill.c
@@ -1,10 +1,10 @@
-//CS1010 Programming Methodology
-//Problem Set 1 Exercise #03: bill.c
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #03: bill.c
 //
-//Program takes in a net price (key-ed in as integer type
-//and converts it to type double for calculation.
-//Program displays service and GST charges
-//Program then displays total bill
+// Program takes in a net price (key-ed in as integer type
+// and converts it to type double for calculation.
+// Program displays service and GST charges
+// Program then displays total bill
 
 #include <stdio.h>
 
@@ -13,21 +13,20 @@ int main(void) {
 	double service, gst, total;
 
 	printf("Enter menu price: $");
-	scanf("%d",&price);
+	scanf("%d", &price);
 	
-	//Calculate service charge, make this answer into double type
-	service = (double)(0.1*price);
-	printf("10%% Service Charge: $%.2lf\n",service);
+	// Calculate service charge, make this answer into double type
+	service = (double)(0.1 * price);
+	printf("10%% Service Charge: $%.2lf\n", service);
 
-	//Calculate GST, make this answer into double type
-	gst = (double)0.07*(price+service);
-	printf("7%% GST: $%.2lf\n",gst);
+	// Calculate GST, make this answer into double type
+	gst = (double) (0.07 * (price + service));
+	printf("7%% GST: $%.2lf\n", gst);
 	
-	//Calculate total bill,comprising of price, service and gst
-	//Make price a double type from integer type
-	total = (double)(price+service+gst);
-	printf("Your total bill: $%.2lf\n",total);
+	// Calculate total bill,comprising of price, service and gst
+	// Make price a double type from integer type
+	total = (double)(price + service + gst);
+	printf("Your total bill: $%.2lf\n", total);
 
 	return 0;
 }
-
