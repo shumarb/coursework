021ee42e7bf0236ee4bfe592b60aab2f5ab5cc71
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-04-08 12:11:43 +0800

    Update travel.c

--- cs1010/problem-set-1/exercise-12/travel.c
+++ cs1010/problem-set-1/exercise-12/travel.c
@@ -7,8 +7,6 @@
 // Program calculates distance from NTUC to Home
 // Program adds the 3 distances above to form total distance
 // Program prints out total distance travelled
-//
-// SHEIKH UMAR
 
 #include <stdio.h>
 #include <math.h>
@@ -17,10 +15,10 @@
 double distance(double x1, double y1, double x2, double y2);
 
 int main(void) {
-	double x1,y1;//Coordinates for Home
-	double x2,y2;//Coordinates for Office
-	double x3,y3;//Coordinates for NTUC
-	double total;//Total distance travelled
+	double x1, y1;	//Coordinates for Home
+	double x2, y2;	//Coordinates for Office
+	double x3, y3;	//Coordinates for NTUC
+	double total;	//Total distance travelled
 	double first_distance, second_distance, third_distance;
 
 	printf("Enter X Y coordinates for Home: ");
@@ -33,16 +31,16 @@ int main(void) {
 	scanf("%lf %lf", &x3, &y3);
 	
 	//Calculate distance from Home to Office
-	first_distance = distance(x1,y1,x2,y2);
+	first_distance = distance(x1, y1, x2, y2);
 	
 	//Calculate distance from Office to NTUC
-	second_distance = distance(x2,y2,x3,y3);
+	second_distance = distance(x2, y2, x3, y3);
 
 	//Calculate distance from NTUC to Home
-	third_distance = distance(x3,y3,x1,y1);
+	third_distance = distance(x3, y3, x1, y1);
 
 	//Calculate total distance
-	total = first_distance+second_distance+third_distance;
+	total = first_distance + second_distance + third_distance;
 	printf("Distance of travel is %.2lf\n", total);
 
 	return 0;
@@ -50,7 +48,7 @@ int main(void) {
 
 // Calculate distance between two points (x1, y1), (x2, y2)
 double distance(double x1, double y1, double x2, double y2) {
-	double travel = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
+	double travel = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 	return travel;
 }
 
