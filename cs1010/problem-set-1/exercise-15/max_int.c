b5c4bbd7178fd3101fe84b4f2980e2fd9bafbb9c
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-04-12 12:04:55 +0800

    Update max_int.c

--- cs1010/problem-set-1/exercise-15/max_int.c
+++ cs1010/problem-set-1/exercise-15/max_int.c
@@ -4,39 +4,38 @@
 // Program reads in 3 integers, passes it to the get_max function.
 // The 3 numbers are compared in the get_max function, which then returns the largest integer back to main function
 // Finally, main program prints out the largest integer
-//
-// SHEIKH UMAR
 
 #include <stdio.h>
 
 // function prototype
 int get_max(int num1, int num2, int num3);
 
-int main(void) {
-
+int main(void)
+{
 	int num1, num2, num3;
 
 	printf("Enter three integers: ");
-	scanf("%d %d %d",&num1,&num2,&num3);
+	scanf("%d %d %d", &num1, &num2, &num3);
 
-	//Pass the integers to get_max function for comparison
-	get_max(num1,num2,num3);
-
-	printf("Maximum input integer is %d\n",get_max(num1,num2,num3));
+	printf("Maximum input integer is %d\n", get_max(num1, num2, num3));
 
 	return 0;
 }
 
 // Return the max value among the three inputs
-int get_max(int num1, int num2, int num3) {
-	if((num1>num2) && (num1>num3)){
-		return num1;//Return num1 if it is the largest integer among the integers
+int get_max(int num1, int num2, int num3)
+{
+	if((num1 > num2) && (num1 > num3))
+	{
+		return num1;
 	}
-	else if((num2>num1) && (num2>num3)){
-		return num2;//Return num2 if it is the largest integer among the integers
+	else if((num2 > num1) && (num2 > num3))
+	{
+		return num2; 
 	}
-	else{
-		return num3;//num3 is the largest integer if the above 2 if-else conditions are not satisfied
+	else
+	{
+		return num3;
 	}
 }
 
