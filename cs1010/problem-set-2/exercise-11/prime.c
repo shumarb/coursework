13e575a8f12065aa4822fc469eb92f14c8b9f26b
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-04-30 12:49:52 +0800

    Update prime.c

--- cs1010/problem-set-2/exercise-11/prime.c
+++ cs1010/problem-set-2/exercise-11/prime.c
@@ -6,46 +6,52 @@
 // If yes, number is not prime.
 // If no, or number is 1, number is not prime.
 // Print out outcome on whether number is a prime.
-// SHEIKH UMAR
 
 #include <stdio.h>
 
 int is_prime(int num);
 
-int main(void) {
+int main(void)
+{
 	int num;
-	int answer;
 
 	printf("Enter a positive integer: ");
-	scanf("%d",&num);
+	scanf("%d", &num);
 
-	//Number is not prime if it is 1.
-	if(num==1){
+	// Number is not prime if it is 1.
+	if(num == 1)
+	{
 		printf("%d is not a prime\n",num);
 	}
-	else{
-		is_prime(num);
+	else
+	{
 		answer = is_prime(num);
 		
-		if(answer==0){
-			printf("%d is a prime\n",num);
+		if(is_prime(num) == 0)
+		{
+			printf("%d is a prime\n", num);
 		}
-		else{
-			printf("%d is not a prime\n",num);
+		else
+		{
+			printf("%d is not a prime\n", num);
 		}
 	}
 
 	return 0;
 }
 
-// <Replace this with a short function description>
-int is_prime(int num) {
+// Function checks if a number is a prime number,
+// and returns a non-negative value.
+int is_prime(int num)
+{
 	int i;
-	int counter=0;
+	int counter = 0;
 
-	for(i=2;i<=(num-1);i++){
-		//Number is a prime if it leaves a remainder (modulo division = 0)
-		if((num%i)==0){
+	for(i = 2; i <= (num - 1); i++)
+	{
+		// Number is a prime if it leaves a remainder (modulo division = 0)
+		if((num % i) == 0)
+		{
 			counter++;
 		}
 	}
