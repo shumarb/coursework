f410c07e30d08ba2e300ba04d874f3addcdf81ab
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-05-02 18:49:14 +0800

    Update count_primes.c

--- cs1010/problem-set-2/exercise-12/count_primes.c
+++ cs1010/problem-set-2/exercise-12/count_primes.c
@@ -3,54 +3,62 @@
 //
 // Program reads in a limit, prints out number of primes
 // from 1 to limit.
-//
-// SHEIKH UMAR
+
 #include <stdio.h>
 #include <math.h>
+
 int is_prime(int num);
 
-//int count_primes(int limit);
+// int count_primes(int limit);
 
-int main(void) {
+int main(void)
+{
 	int i;
 	int limit;
-	int primes=0;
+	int primes = 0;
 
 	printf("Enter limit: ");
-	scanf("%d",&limit);
-	//1 is not a prime.
-	if(limit==1){
-		printf("Number of primes (1-%d): 0\n",limit);
+	scanf("%d", &limit);
+	
+	// 1 is not a prime.
+	if(limit == 1)
+	{
+		printf("Number of primes (1-%d): 0\n", limit);
 	}
-	//Hardcode 2, as 2%1==2%2==0, Hence it is a prime.
-	else if(limit==2){
-		printf("Number of primes (1-%d): 1\n",limit);
+	
+	// Hardcode 2, as 2 % 1 == 2 % 2 == 0, Hence it is a prime.
+	else if(limit == 2)
+	{
+		printf("Number of primes (1-%d): 1\n", limit);
 	}
-	else{
-		for(i=3;i<=limit;i++){
-			is_prime(i);
-			if(is_prime(i)==0){
+	else
+	{
+		for(i = 3; i <= limit; i++)
+		{
+			if(is_prime(i) == 0)
+			{
 				primes++;
 			}
 		}
-		primes++;//Include 2, if limit>2
-		printf("Number of primes (1-%d): %d\n",limit,primes);
+		primes++; // Include 2, if limit > 2
+		printf("Number of primes (1-%d): %d\n", limit, primes);
 	}
 
 	return 0;
 }
-// Return 1 if num is a prime, 0 otherwise
-int is_prime(int num){
+// Return 1 if num is not a prime, 0 otherwise
+int is_prime(int num)
+{
 	int j;
-	int non_prime=0;//non_prime=0 is for prime number
 
-	for(j=2;j<=sqrt(num);j++){
-		if((num%j)==0){
-			non_prime=1;//Num is divisible by a number other than 1 and itself.
-									//Num is not a prime.
+	for(j = 2; j <= sqrt(num); j++)
+	{
+		if((num % j) == 0)
+		{
+			return 1; // Num is divisible by a number other than 1 and itself, hence num is not a prime.
 		}
 	}
-	return non_prime;
+	return 0;
 }
 
 
