377500a468cda5659d0e85e59c8f276accd46020
Sheikh Umar <sheikh.umar.banafe@u.nus.edu>
2020-05-06 12:50:41 +0800

    Update print_square.c

--- cs1010/problem-set-2/exercise-16/print_square.c
+++ cs1010/problem-set-2/exercise-16/print_square.c
@@ -3,35 +3,37 @@
 //
 // Program reads in a number, and prints out a square
 // that is of the size of the number.
-//
-// SHEIKH UMAR, A0149874L
 
 #include <stdio.h>
 
 int main(void) {
-	int k;
-	int i,j;
+	int i, j, k
 
 	printf("Enter k: ");
-	scanf("%d",&k);
+	scanf("%d", &k);
 
-	// Case 1: k=2
-	// Must hardcode as square where k=2 has no space
-	if(k==2){
+	// Case 1: k = 2
+	// Must hardcode as square where k = 2 has no space
+	if(k == 2)
+	{
 		printf("**\n");
 		printf("**\n");
 	}
-	else{
+	else
+	{
 		// First line
-		for(i=1;i<=k;i++){
+		for(i = 1; i <= k; i++)
+		{
 			printf("*");
 		}
 		printf("\n");
 
-		//Second line to second-last line
-		for(i=2;i<=(k-1);i++){
+		// Second line to second-last line
+		for(i = 2; i <= (k-1); i++)
+		{
 			printf("*");
-			for(j=2;j<=(k-1);j++){
+			for(j = 2; j <= (k-1); j++)
+			{
 				printf(" ");
 			}
 			printf("*");
@@ -39,11 +41,13 @@ int main(void) {
 		}
 
 		// print last line
-		for(i=1;i<=k;i++){
+		for(i = 1; i <= k; i++)
+		{
 			printf("*");
 		}
 		printf("\n");
 	}
+	
 	return 0;
 }
 
