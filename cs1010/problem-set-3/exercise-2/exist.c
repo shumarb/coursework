bce72391ac0e07311c80a77b67a84053b91382cb
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2020-05-11 00:38:01 +0800

    Added code and test data for exercises 2 and 3

---
+++ cs1010/problem-set-3/exercise-2/exist.c
@@ -0,0 +1,58 @@
+// CS1010 Programming Methodology
+// Problem Set 3 Exercise #02: exist.c
+//
+// This program reads an array of integers
+// and another integer k, checks if k exists
+// in the array.
+
+#include <stdio.h>
+#define MAX_SIZE 10
+
+int contains(int arr[], int size, int k);
+
+int main(void)
+{
+	// define array according to the max size
+	int i, k, size;
+	int arr[MAX_SIZE];
+	
+	printf("Enter the number of elements: ");
+	scanf("%d", &size); // read size of array
+	
+	printf("Enter %d elements: ", size);
+	for(i = 0; i < size; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printf("Enter k: ");
+	scanf("%d", &k);
+
+	if(contains(arr, size, k) == 1)
+	{
+		printf("%d is in the given array\n", k);
+	}
+	else
+	{
+		printf("%d is not in the given array\n", k);
+	}
+
+	return 0;
+}
+
+// Take an array 'arr' of 'size' elements,
+// check if k exists in the array.
+// Return 1 if so, 0 otherwise
+int contains(int arr[], int size, int k)
+{
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		if(arr[i] == k)
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
