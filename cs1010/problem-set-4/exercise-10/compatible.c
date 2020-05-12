303505db4a84c9f65c9932ef3a21ce70831b4a47
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2020-05-13 00:13:47 +0800

    Added code & test data for problem sets 3 and 4

---
+++ cs1010/problem-set-4/exercise-10/compatible.c
@@ -0,0 +1,102 @@
+// CS1010 Programming Methodology
+// Problem Set 4 Exercise #10: compatible.c
+//
+// This program removes common characters
+// in two names, ignoring cases. Then check
+// whether the remains two strings both have
+// odd or even characters.
+//
+// SHEIKH UMAR A0149874L
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// Name has at most 35 characters
+#define MAXLEN 36
+
+void read_name(char name[]);
+void match(char name1[], char name2[], int *count1_p, int *count2_p);
+
+int main(void)
+{
+	char name_1[MAXLEN], name_2[MAXLEN];
+	int remain_1, remain_2;// number of remaining letters in name1 and name2 after matching
+
+	printf("Enter 2 names below:\n");
+	read_name(name_1); // do not remove/change this line
+	read_name(name_2); // do not remove/change this line
+
+	// Call the match() function here
+	match(name_1, name_2, &remain_1, &remain_2);
+
+	printf("Number of letters remaining in 1st name = %d\n", remain_1);
+	printf("Number of letters remaining in 2nd name = %d\n", remain_2);
+
+	if(((remain_1 % 2) == 0 && (remain_2 % 2) == 0) || ((remain_1 % 2) != 0 && (remain_2 % 2) != 0))
+	{
+		printf("The names are compatible.\n");
+	}
+	else
+	{
+		printf("The names are not compatible.\n");
+	}
+
+	return 0;
+}
+
+// Read a name (may contain spaces) using fgets
+void read_name(char name[])
+{
+	fgets(name,MAXLEN,stdin);
+	int j;
+
+	if(name[strlen(name)] == '\n')
+	{
+		name[strlen(name)] = '\0';
+	}
+
+	for(j = 0; j < strlen(name); j++)
+	{
+		name[j] = tolower(name[j]);
+	} 
+}
+
+// We cross out common characters from both names,
+// and return the number of remaining letters in both names.
+void match(char name_1[], char name_2[], int *count1_p, int *count2_p)
+{
+	*count1_p = 0;
+	*count2_p = 0;
+	int j, k, done = 0;
+
+	for(j = 0; j < strlen(name_1); j++)
+	{
+		done = 0;
+		for(k = 0; k < strlen(name_2) && done == 0; k++)
+		{
+			if(name_1[j] == name_2[k])
+			{
+				name_1[j] = name_2[k] = '!';
+				done = 1;
+			}
+		}
+	}
+
+	for(j = 0; j < strlen(name_1); j++)
+	{
+		if(isalpha(name_1[j]))
+		{
+			(*count1_p)++;
+		}
+	}
+
+	for(j = 0; j < strlen(name_2); j++)
+	{
+		if(isalpha(name_2[j]))
+		{
+			(*count2_p)++;
+		}
+	}
+}
+
---
+++ cs1010/problem-set-4/exercise-4/print_copies.c
@@ -0,0 +1,32 @@
+// CS1010 Programming Methodology
+// Problem Set 4 Exercise #04: print_copies.c
+//
+// This program prints duplicated copies of an
+// input string, up to a total of 10 characters.
+//
+// SHEIKH UMAR A01409874L
+
+#include <stdio.h>
+#include <string.h>
+#define LEN 6 
+
+int main(void)
+{
+	char input[LEN];
+	int i, input_len, rounds;
+
+	printf("Enter a string: ");
+	scanf("%s", input);
+
+	input_len = strlen(input);
+	rounds = 10 / input_len;
+
+	for(i = 0 ; i < rounds; i++)
+	{
+		printf("%s",input);
+	}
+	printf("\n");
+
+	return 0;
+}
+
