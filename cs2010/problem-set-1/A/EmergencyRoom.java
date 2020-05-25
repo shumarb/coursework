27e339af80151e13648318d97d03524d9d36bdc7
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2020-05-26 00:04:52 +0800

    Amended code in CS2010 problem set 1.

--- cs2010/problem-set-1/A/EmergencyRoom.java
+++ cs2010/problem-set-1/A/EmergencyRoom.java
@@ -2,52 +2,52 @@
 import java.util.*;
 import java.io.*;
 
-class Patient implements Comparable <Patient>{
+class Patient implements Comparable <Patient> {
     private String _name;
     private int _level;
     private int _position;
 
-    public Patient(String name, int level, int position){
+    public Patient(String name, int level, int position) {
         this._name = name;
         this._level = level;
         this._position = position;
     }   
 
-    public String getName(){
+    public String getName() {
         return _name;
     }
 
-    public int getLevel(){
+    public int getLevel() {
         return _level;
     }
 
-    public int getPosition(){
+    public int getPosition( ){
         return _position;
     }
 
-    public int compareTo(Patient incoming){
-        if( incoming._level < this._level ){
+    public int compareTo(Patient incoming) {
+        if(incoming._level < this._level) {
             return -1;
         }
-        else if( incoming._level > this._level ){
+        else if(incoming._level > this._level) {
             return 1;
         }
-        else{
-            if( incoming._position < this._position ){
+        else {
+            if(incoming._position < this._position) {
                 return 1;
             }
-            else if( incoming._position > this._position ){
+            else if(incoming._position > this._position) {
                 return -1;
             }
-            else{
+            else {
                 return 0;
             }
         }
     }
 }
 
-public class EmergencyRoom{
-    private boolean test = true;
+public class EmergencyRoom {
+    private boolean isTest = true;
     private TreeMap <String, Integer> patientsLevel = new TreeMap <String, Integer>();
     private TreeMap <String, Integer> patientsPosition = new TreeMap <String, Integer> ();
     private TreeSet <Patient> patientsTree = new TreeSet <Patient>();
@@ -58,12 +58,12 @@ public class EmergencyRoom{
      * Precon: test==true.
      * Postcon: Nil.
      */
-    private void displaysInformation(){
+    private void displaysInformation() {
         System.out.println("patientsLevel: " + patientsLevel);
         System.out.println("patientsPosition: " + patientsPosition);
         System.out.println("patientsTree: ");
         int count = 1;
-        for(Patient curr: patientsTree){
+        for(Patient curr: patientsTree) {
             System.out.print("Patient " + count++ + ": ");
             System.out.println(curr.getName() + ", " + curr.getLevel() + ", " + curr.getPosition());
         }
@@ -75,7 +75,7 @@ public class EmergencyRoom{
      * Precon: User entered option 0.
      * Postcon: Patient's information is stored in the hashmaps.
      */
-    private Patient createsPatient(String patientName, int emergencyLvl){
+    private Patient createsPatient(String patientName, int emergencyLvl) {
         patientsLevel.put(patientName, emergencyLvl);
         patientsPosition.put(patientName, count);
         Patient incoming = new Patient(patientName, emergencyLvl, count++);
@@ -87,14 +87,14 @@ public class EmergencyRoom{
      * Precon: Patient's info is stored in 2 treemaps
      * Postcon: Patient is added into treeset
      */
-    private void storesPatient(Patient incoming){
+    private void storesPatient(Patient incoming) {
         patientsTree.add(incoming);
     }
 
     void ArriveAtHospital(String patientName, int emergencyLvl) {
         Patient incoming = createsPatient(patientName, emergencyLvl);
         storesPatient(incoming);
-        if( test ){
+        if(isTest) {
             displaysInformation();
         }
     }
@@ -104,8 +104,8 @@ public class EmergencyRoom{
      * Precon: User entered option 1 or 2
      * Postcon: Patient is removed from treeset
      */
-    private void removesPatient(Patient toRemove){
-        if( test ){
+    private void removesPatient(Patient toRemove) {
+        if(isTest) {
             System.out.print("To remove: ");
             System.out.println(toRemove.getName() + ", " + toRemove.getLevel() + ", " + toRemove.getPosition());
             System.out.println("Before removal: ");
@@ -114,7 +114,7 @@ public class EmergencyRoom{
         patientsTree.remove(toRemove);
         patientsPosition.remove(toRemove.getName());
         patientsLevel.remove(toRemove.getName());
-        if( test ){
+        if(isTest) {
             System.out.println("After removal: ");
             displaysInformation();
         }
@@ -124,14 +124,14 @@ public class EmergencyRoom{
      * Precon: User entered option 1
      * Postcon: Patient that user entered has been removed
      */
-    private Patient formsToRemove(String patientName){
+    private Patient formsToRemove(String patientName ){
         int position = patientsPosition.get(patientName);
         int level = patientsLevel.get(patientName);
         Patient toRemove = new Patient(patientName, level, position);
         return toRemove;
     }
 
-    void Treat(String patientName){
+    void Treat(String patientName) {
         Patient toRemove = formsToRemove(patientName);        
         removesPatient(toRemove);
     }
@@ -141,7 +141,7 @@ public class EmergencyRoom{
      * Precon: User entered option 2
      * Postcon: Patient level has been updated
      */
-    private int updatesLevel(String patientName, int incEmergencyLvl){
+    private int updatesLevel(String patientName, int incEmergencyLvl) {
         int updatedVal = incEmergencyLvl + (patientsLevel.get(patientName)).intValue();
         return updatedVal;
     }
@@ -151,15 +151,15 @@ public class EmergencyRoom{
      * Precon: User entered option 2
      * Postcon: Patient has been removed from treeset
      */
-    private void removesPatientV2(Patient toUpdate){
-        if( test ){
+    private void removesPatientV2(Patient toUpdate) {
+        if(isTest) {
             System.out.print("To remove: ");
             System.out.println(toUpdate.getName() + ", " + toUpdate.getLevel() + ", " + toUpdate.getPosition());
             System.out.println("Before removal: ");
             displaysInformation();
         }
         patientsTree.remove(toUpdate);
-        if( test ){
+        if(isTest) {
             System.out.println("After removal: ");
             displaysInformation();
         }
@@ -170,19 +170,19 @@ public class EmergencyRoom{
      * Precon: User entered option 1.
      * Postcon: Upated patient is added into treeset
      */
-    private void storesPatientV2(String patientName, int updatedVal, int originalPosition){
+    private void storesPatientV2(String patientName, int updatedVal, int originalPosition) {
         Patient updated = new Patient(patientName, updatedVal, originalPosition);
         patientsTree.add(updated);
-        if( test ){
+        if(isTest) {
             System.out.println("After update: ");
             displaysInformation();
         }
     }
 
-    void UpdateEmergencyLvl(String patientName, int incEmergencyLvl){
+    void UpdateEmergencyLvl(String patientName, int incEmergencyLvl) {
         int updatedVal = updatesLevel(patientName, incEmergencyLvl);
         int originalPosition = patientsPosition.get(patientName);
-        if( test ){
+        if(isTest) {
             System.out.println("UpdatedVal = " + updatedVal + ", originalPosition = " + originalPosition);
         }
         Patient toUpdate = formsToRemove(patientName);
@@ -191,13 +191,13 @@ public class EmergencyRoom{
         storesPatientV2(patientName, updatedVal, originalPosition);
     }
 
-    String Query(){
-        if( patientsTree.isEmpty() ){
+    String Query() {
+        if(patientsTree.isEmpty()) {
             return "The emergency suite is empty";
         }
-        else{
+        else {
             Patient highest = patientsTree.first();
-            if( test ){
+            if(isTest) {
                 System.out.print("Highest: ");
                 System.out.println(highest.getName() + ", " + highest.getLevel() + ", " + highest.getPosition());
             }
@@ -205,13 +205,13 @@ public class EmergencyRoom{
         }
     }
 
-    void run() throws Exception{
+    void run() throws Exception {
         // do not alter this method
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
         int numCMD = Integer.parseInt(br.readLine()); // note that numCMD >= N
-        while (numCMD-- >0 ){
+        while (numCMD-- >0 ) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int command = Integer.parseInt(st.nextToken());
             switch (command) {
@@ -224,9 +224,10 @@ public class EmergencyRoom{
         pr.close();
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         // do not alter this method
         EmergencyRoom ps1 = new EmergencyRoom();
         ps1.run();
     }
 }
+
