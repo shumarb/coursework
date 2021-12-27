d4f5079dd9db58d823b5df754b9b8689bdb3e7c9
Sheikh-Umar <sheikh.umar.banafe@u.nus.edu>
2021-12-28 00:55:50 +0800

    Updated code for cs2010 problem set 0 exercise a

--- cs2010/problem-set-0/a/Addition.java
+++ cs2010/problem-set-0/a/Addition.java
@@ -2,31 +2,29 @@
 // Read in 2 numbers, and print out its sum.
 // Keep asking user for the number as long as both are at least 0.
 
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
-public class Addition{
-    private Scanner sc = new Scanner(System.in);
-    private boolean test = false;
+public class Addition {
+    private boolean isTest = false;
 
-    public static void main(String[] args){
-        Addition obj = new Addition();
-        obj.run();
-    }
-
-    private void run(){
-        int num1, num2;
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
-        if( test ){
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt(), num2 = sc.nextInt();
+        if (isTest) {
             System.out.println("num1 = " + num1 + ", num2 = " + num2);
         }
 
-        while( num1>=0 || num2>=0 ){
-            int sum = num1+num2;
-            System.out.println(sum);
+        while (num1 >= 0 || num2 >= 0) {
+            System.out.println(num1 + num2);
             num1 = sc.nextInt();
             num2 = sc.nextInt();
         }
+
+        sc.close();
+    }
+    
+    public static void main(String[] args){
+        Addition obj = new Addition();
+        obj.run();
     }
 }
