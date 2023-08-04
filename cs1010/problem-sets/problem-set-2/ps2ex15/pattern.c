053683e515ca5bb1759ecbcb7a5196461163e634
Sheikh-Umar <sheikh.umar.banafe@outlook.com>
2023-08-05 00:28:04 +0800

    Added solutions and pdf of exercises for problem set 2

---
+++ cs1010/problem-sets/problem-set-2/ps2ex15/pattern.c
@@ -0,0 +1,31 @@
+// CS1010 Programming Methodology
+// Problem Set 2 Exercise #15: pattern.c
+//
+// Program reads in a number, and prints out a pattern
+// that corresponds to the number.
+//
+// SHEIKH UMAR
+
+#include <stdio.h>
+
+int main(void)
+{
+	int count, i, j, k = 0; 
+
+	printf("Enter count: ");
+	scanf("%d", &count);
+
+	for(i = 1; i <= count; i++)
+	{
+		printf("#");
+		k++;
+		for(j = 1; j <=k; j++)
+		{
+			printf("-");
+		}
+	}
+	printf("\n");
+	
+	return 0;
+}
+
---
+++ cs1010/problem-sets/problem-set-2/ps2ex25/prime_product.c
@@ -0,0 +1,52 @@
+// CS1010 Programming Methodology
+// Problem Set 2 Exercise #25: prime_product.c
+//
+// Program reads in a number,
+// and prints it out as a product of its prime multiples in increasing order.
+//
+// SHEIKH UMAR
+#include <stdio.h>
+
+void calculate(int num);
+
+int main(void) 
+{
+	int num;
+	printf("Enter integer: ");
+	scanf("%d", &num);
+	calculate(num);
+	
+	return 0;
+}
+
+// Calculate and print out num as a multiple of its primes.
+void calculate(int num){
+	int i = 2;
+	
+	if(num < 0)
+	{
+		printf("%d = -1", num);
+	}
+	if(num > 0)
+	{
+		printf("%d = 1", num);
+	}
+
+	if(num < 0)
+	{
+		num*=-1;
+	}
+	
+	int k = num;
+	
+	while(k > 1)
+	{
+		while((k % i) != 0)
+		{
+			i++;
+		}
+		k /= i;
+		printf(" * %d", i);
+	}
+	printf("\n");
+}
