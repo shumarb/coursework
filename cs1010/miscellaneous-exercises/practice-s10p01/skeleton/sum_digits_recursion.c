96f291b46e8ff69891d13d80ac4e8bdfd1afc21c
Sheikh-Umar <sheikh.umar.banafe@outlook.com>
2023-08-06 00:27:59 +0800

    added exercise1 on recursion

---
+++ cs1010/miscellaneous-exercises/practice-s10p01/skeleton/sum_digits_recursion.c
@@ -0,0 +1,26 @@
+// sum_digits_recursion.c
+// This program reads a non-negative integer and calls
+// a recursive function to sum up all digits in that integer.
+#include <stdio.h>
+
+int sumDigits(int);
+
+int main(void) {
+	int num;
+
+	printf("Enter a non-negative integer: ");
+	scanf("%d", &num);
+
+	printf("Sum of its digits = %d\n", sumDigits(num));
+
+	return 0;
+}
+
+// Return sum of digits in n
+// Precond: n >= 0
+int sumDigits(int n) {
+
+
+	return 123;
+}
+
