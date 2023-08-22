10d9474274594c23638b085840458081f4dd40cc
Sheikh-Umar <sheikh.umar.banafe@outlook.com>
2023-08-23 00:04:09 +0800

    added code, testdata, and pdf for practice-s03p09

---
+++ cs1010/miscellaneous-exercises/practice-s03p09/skeleton/check_order.c
@@ -0,0 +1,13 @@
+// check_order.c 
+// Repeatedly reads positive integer, until the input value 
+// is zero, or the data are not in increasing order.
+
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Enter positive integer: ");
+
+	return 0;
+}
+
