a25c387aba8e08bc9b205c83d1322591bce5de71
Sheikh-Umar <sheikh.umar.banafe@outlook.com>
2023-08-24 00:47:29 +0800

    added code, testdata, and pdf for practice-s04p01

---
+++ cs1010/miscellaneous-exercises/practice-s04p01/skeleton/century.c
@@ -0,0 +1,17 @@
+// CS1010 AY2011/2 Semester 1 
+// PE1 Ex1: century.c
+// Name: 
+// Matriculation number: 
+// plab account-id: 
+// Discussion group: 
+// Description:
+
+int main(void)
+{
+	int year; // user's input
+
+	printf("Enter year: ");
+
+	return 0;
+}
+
