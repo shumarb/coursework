a63ba5eb0b89799a2f238a563222f29ebe892381
Sheikh-Umar <sheikh.umar.banafe@outlook.com>
2023-08-25 00:14:12 +0800

    added code, testdata, skeleton code, and pdf for practice-s04p02

---
+++ cs1010/miscellaneous-exercises/practice-s04p02/skeleton/winners.c
@@ -0,0 +1,19 @@
+// CS1010 AY2011/2 Semester 1 
+// PE1 Ex2: winners.c
+// Name: 
+// Matriculation number: 
+// plab account-id: 
+// Discussion group: 
+// Description: 
+
+int main(void)
+{
+	int factor;       
+
+	printf ("Enter factor-digit: ");
+	printf("Enter must-have-digit: ");
+	printf("Enter number of participants: ");
+
+	return 0;
+}
+
