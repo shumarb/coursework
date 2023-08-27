61c7cc8f61d99156b6a87307997590b854ccac40
Sheikh-Umar <sheikh.umar.banafe@outlook.com>
2023-08-27 15:10:43 +0800

    added testdata, skeleton code, code, pdf for practice-s04p04

---
+++ cs1010/miscellaneous-exercises/practice-s04p04/skeleton/ucard.c
@@ -0,0 +1,16 @@
+// CS1010 AY2012/3 Semester 1
+// PE1 Ex2: ucard.c
+// Name: 
+// Matriculation number: 
+// plab account-id: 
+// Discussion group: 
+// Description:
+
+int main(void) {
+	int uCardNo; 
+
+	printf("Enter uCard Number: ");
+
+	return 0;
+}
+
