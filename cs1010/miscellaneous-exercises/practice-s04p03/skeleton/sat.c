c5fac88bb52fd9d45f8e15d88ea234a28290e540
Sheikh-Umar <shumarb@outlook.com>
2023-09-06 11:41:37 +0800

    added code, pdf, skeleton code, and testdata for practice-s04p03

---
+++ cs1010/miscellaneous-exercises/practice-s04p03/skeleton/sat.c
@@ -0,0 +1,17 @@
+// CS1010 AY2012/3 Semester 1
+// PE1 Ex1: sat.c
+// Name: 
+// Matriculation number: 
+// plab account-id: 
+// Discussion group: 
+// Description:
+
+int main(void) {
+
+	int verbal, math, writing ; // user's input
+
+	printf("Enter scores: ");
+
+	return 0;
+
+}
