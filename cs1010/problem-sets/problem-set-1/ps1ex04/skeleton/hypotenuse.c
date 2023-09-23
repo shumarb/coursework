493787b886145a876c6a441e123dc6c8a2e23baa
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 12:10:43 +0800

    updated cs1010 problem set 1 exercise 5 to ensure it contains code, pdf, skeleton code, and testdata (input & output)

---
+++ cs1010/problem-sets/problem-set-1/ps1ex04/skeleton/hypotenuse.c
@@ -0,0 +1,19 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #04: hypotenuse.c
+//
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+#include <math.h> // to use sqrt() function
+
+int main(void) {
+
+	printf("Enter a and b: ");
+
+	printf("c = ");
+
+	return 0;
+}
