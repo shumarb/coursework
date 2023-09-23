5daed908940efa76183102ac5399f8a2854cc52a
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 12:13:23 +0800

    updated cs1010 problem set 1 exercise 5 to ensure it contains code, pdf, skeleton code, and testdata (input & output)

---
+++ cs1010/problem-sets/problem-set-1/ps1ex05/skeleton/investment.c
@@ -0,0 +1,23 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #05: investment.c
+//
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+	printf("Enter principal amount: ");
+
+	printf("Enter interest rate   : ");
+
+	printf("Enter number of years : ");
+
+	printf("Amount = $");
+
+	return 0;
+}
