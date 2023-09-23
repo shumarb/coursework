c2ed7ef1385fbc254a92d1ba214c34513e4bb996
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 12:24:04 +0800

    updated problem set 1 exercise 6 to ensure it contains code, pdf, skeleton code, and testdata (input & output)

---
+++ cs1010/problem-sets/problem-set-1/ps1ex06/skeleton/coins.c
@@ -0,0 +1,18 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #06: coins.c
+//
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Enter amount in cents: ");
+
+	printf("Minimum number of coins needed: ");
+
+	return 0;
+}
