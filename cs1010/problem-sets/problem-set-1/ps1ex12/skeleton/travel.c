13028ee884957acdf23d58792ab1c6ba3dbf2998
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 13:26:58 +0800

    updated cs1010 problem set 1 exercise 12 to ensure it contains code, pdf, skeleton code, and testdata (input & output)

---
+++ cs1010/problem-sets/problem-set-1/ps1ex12/skeleton/travel.c
@@ -0,0 +1,33 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #12: travel.c
+//
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+#include <math.h>
+
+// function prototype
+double distance(double x1, double y1, double x2, double y2);
+
+int main(void) {
+
+
+	printf("Enter X Y coordinates for Home: ");
+
+	printf("Enter X Y coordinates for Office: ");
+
+	printf("Enter X Y coordinates for NTUC: ");
+
+	printf("Distance of travel is ");
+
+	return 0;
+}
+
+// Calculate distance between two points (x1, y1), (x2, y2)
+double distance(double x1, double y1, double x2, double y2) {
+
+	return 0;  // stub, to be replaced by your code
+}
