235d6350d083424dc5a63606d15cfd92d49ad703
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 12:05:42 +0800

    updated cs1010 problem set 1 exercise 3 to ensure it contains code, pdf, skeleton code, and testdata (input & output)

---
+++ cs1010/problem-sets/problem-set-1/ps1ex03/bill.c
@@ -0,0 +1,27 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #03: bill.c
+//
+// Program computes total bill based on a menu price
+//
+//
+// SHEIKH UMAR
+
+#include <stdio.h>
+
+int main(void) {
+
+	double gst, net_price, service_charge;
+
+	printf("Enter menu price: $");
+	scanf("%lf", &net_price);
+
+	service_charge = net_price * 0.1;
+	printf("10%% Service Charge: $%.2lf\n", service_charge);
+
+	gst = 0.07 * (net_price + service_charge);
+	printf("7%% GST: $%.2lf\n", gst);
+
+	printf("Your total bill: $%.2lf\n", gst + net_price + service_charge);
+
+	return 0;
+}
---
+++ cs1010/problem-sets/problem-set-1/ps1ex03/skeleton/bill.c
@@ -0,0 +1,22 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #03: bill.c
+//
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Enter menu price: $");
+
+	printf("10%% Service Charge: $");
+
+	printf("7%% GST: $");
+
+	printf("Your total bill: $");
+
+	return 0;
+}
