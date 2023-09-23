ac105cb71b4c62bc935eca36efc5cec79193a62f
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 14:18:36 +0800

    refactored code, and ensured code, pdf, skeleton code, and testdata are all correct

---
+++ cs1010/problem-sets/problem-set-1/ps1ex20/skeleton/younger.c
@@ -0,0 +1,24 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #20: younger.c
+//
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Enter birthday for Tamil (day month year): ");
+
+	printf("Enter birthday for Alice (day month year): ");
+
+	printf("Alice is younger\n");
+
+	printf("Tamil and Alice are of the same age\n");
+
+	printf("Tamil is younger\n");
+
+	return 0;
+}
