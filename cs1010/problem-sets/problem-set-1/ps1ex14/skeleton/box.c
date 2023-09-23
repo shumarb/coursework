387771ec725b7af2fce62fc5da573d9ca8a00501
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 13:40:23 +0800

    updated cs1010 problem set 1 exercise 14 to ensure it contains code, pdf, skeleton code, and testdata (input & output)

---
+++ cs1010/problem-sets/problem-set-1/ps1ex14/skeleton/box.c
@@ -0,0 +1,41 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #14: box.c
+//
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+#include <math.h>
+
+// function prototypes
+int compute_surface_area(int length, int width, int height);
+double compute_diagonal(int length, int width, int height);
+
+int main(void) {
+
+	printf("Enter length: ");
+
+	printf("Enter width : ");
+
+	printf("Enter height: ");
+
+	printf("Surface area = " );
+
+	printf("Diagonal = ");
+
+	return 0;
+}
+
+// Compute surface area
+int compute_surface_area(int length, int width, int height) {
+
+	return 0; // stub, to be replaced by your code
+}
+
+// Compute length of diagonal
+double compute_diagonal(int length, int width, int height) {
+
+	return 0; // stub, to be replaced by your code
+}
