3b01503f7e014b27cc31dc247271d87a2006eac8
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 15:52:21 +0800

    refactored code, and ensured code, pdf, skeleton code, and testdata are correct

---
+++ cs1010/problem-sets/problem-set-1/ps1ex24/skeleton/lifts.c
@@ -0,0 +1,28 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #24: lifts.c
+// 
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+
+int main(void) {
+
+	// first instruction
+	printf("Enter 1st instruction: ");
+
+	// process first instruction
+
+	// second instruction
+	printf("Enter 2nd instruction: ");
+
+	// process second instruction
+
+	// print out results
+	printf("Lift 1 took %d sec. and ended at level %d\n" ); 
+	printf("Lift 2 took %d sec. and ended at level %d\n" );
+
+	return 0;
+}
