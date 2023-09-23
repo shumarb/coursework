1ed103ef316e76f0fbd24007b31fc9053e3d0a42
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 14:11:07 +0800

    code refactoring, and ensured code, pdf, skeleton code, and tesdata are correct

---
+++ cs1010/problem-sets/problem-set-1/ps1ex18/skeleton/triangle.c
@@ -0,0 +1,22 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #18: triangle.c
+//
+// <Replace this with a short program description>
+//
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Equilateral\n");
+
+	printf("Not a triangle\n");
+
+	printf("Isosceles\n");
+
+	printf("Scalene\n");
+
+	return 0;
+}
