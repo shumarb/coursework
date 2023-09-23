d40d25f574cb5aa68787ca19fe9b51b95dda74dd
Sheikh-Umar <shumarb@outlook.com>
2023-09-23 11:49:52 +0800

    updated cs1010 problem set 1 exercise 2 to include code, pdf, skeleton code, testdata (input & output)

---
+++ cs1010/problem-sets/problem-set-1/ps1ex02/skeleton/heights.c
@@ -0,0 +1,23 @@
+// CS1010 Programming Methodology
+// Problem Set 1 Exercise #02: heights.c
+// 
+// This program calculates the heights of
+// children according to heights of parents.
+//
+// <Replace this with your name>
+
+#include <stdio.h>
+
+int main(void) {
+
+	double john, mary, tom, kate;
+
+	printf("Enter the heights for John and Mary: ");
+
+	// fill in your code
+
+	printf("Height for Tom: %.2f\n", tom);
+	printf("Height for Kate: %.2f\n", kate);
+
+	return 0;
+}
